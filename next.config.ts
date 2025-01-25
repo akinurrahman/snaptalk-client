50990a7c74c1655d803d9c7adbe89a1a9403fe0e
Akinur Rahman <akinurrahmanofficial@gmail.com>
2025-01-25 18:34:02 +0530

    login/signup ui done

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "res.cloudinary.com",
+        pathname: "/**", // Allow any path on this hostname
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "/**", // Allow any path on this hostname
+      },
+    ],
+  },
 };
 
 export default nextConfig;
---
+++ src/app/(auth)/components/login-form.tsx
@@ -0,0 +1,75 @@
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import { Card, CardContent } from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import Image from "next/image"
+import Link from "next/link"
+
+export function LoginForm({
+    className,
+    ...props
+}: React.ComponentProps<"div">) {
+    return (
+        <div className={cn("flex flex-col gap-6", className)} {...props}>
+            <Card className="overflow-hidden">
+                <CardContent className="grid p-0 md:grid-cols-2">
+                    <form className="p-6 md:p-8">
+                        <div className="flex flex-col gap-6">
+                            <div className="flex flex-col items-center text-center">
+                                <h1 className="text-2xl font-bold">Welcome back</h1>
+                                <p className="text-balance text-muted-foreground">
+                                    Login to your SnapTalk account
+                                </p>
+                            </div>
+                            <div className="grid gap-2">
+                                <Label htmlFor="email">Email</Label>
+                                <Input
+                                    id="email"
+                                    type="email"
+                                    placeholder="m@example.com"
+                                    required
+                                />
+                            </div>
+                            <div className="grid gap-2">
+                                <div className="flex items-center">
+                                    <Label htmlFor="password">Password</Label>
+                                    <a
+                                        href="#"
+                                        className="ml-auto text-sm underline-offset-2 hover:underline"
+                                    >
+                                        Forgot your password?
+                                    </a>
+                                </div>
+                                <Input id="password" type="password" required />
+                            </div>
+                            <Button type="submit" className="w-full">
+                                Login
+                            </Button>
+                            <div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 hover:[&_a]:text-primary">
+                                By clicking continue, you agree to our <a href="#">Terms of Service</a>{" "}
+                                and <a href="#">Privacy Policy</a>.
+                            </div>
+                          
+                            <div className="text-center text-sm">
+                                Don&apos;t have an account?{" "}
+                                <Link href="/signup" className="underline underline-offset-4">
+                                    Sign up
+                                </Link>
+                            </div>
+                        </div>
+                    </form>
+                    <div className="relative hidden bg-muted md:block">
+                        <Image
+                            src="https://images.unsplash.com/photo-1616960371261-12f51dccb852?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGNoYXR0aW5nfGVufDB8fDB8fHww"
+                            alt="Image"
+                            className="absolute inset-0 h-full w-full object-cover dark:grayscale"
+                            fill
+                        />
+                    </div>
+                </CardContent>
+            </Card>
+            
+        </div>
+    )
+}
